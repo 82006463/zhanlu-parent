383e0c9edf0da86b7c6e6d8491b882b326d95540
82006463 <82006463@163.com>
2017-08-27 13:31:31 +0800

    update pom.xml

--- zhanlu-core/src/main/java/com/zhanlu/core/Main.java
+++ zhanlu-core/src/main/java/com/zhanlu/core/Main.java
@@ -1,8 +1,11 @@
 package com.zhanlu.core;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Created by Administrator on 2017/8/27.
  */
+@Slf4j
 public class Main {
 
     public static void main(String[] args) {
