e5d22c441872653ad3e157357bb865b8ee0bb363
82006463 <82006463@163.com>
2017-08-27 13:41:40 +0800

    update Main.class

---
+++ zhanlu-core/src/main/java/com/zhanlu/boot/Main.java
@@ -0,0 +1,43 @@
+package com.zhanlu.boot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by Administrator on 2017/8/27.
+ */
+@Slf4j
+@SpringBootApplication
+public class Main {
+
+    private static final CountDownLatch latch = new CountDownLatch(1);
+
+    public static void main(String[] args) {
+        Main.run(Main.class, args);
+    }
+
+    public static ConfigurableApplicationContext run(Class<?> clazz, String[] args) {
+        ConfigurableApplicationContext ctx = null;
+        try {
+            ctx = SpringApplication.run(Main.class, args);
+        } catch (Exception e) {
+            log.error("Main启动失败", e);
+            ctx.close();
+        }
+
+        if (ctx.containsBean("dispatcherServlet")) {
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                ctx.close();
+                e.printStackTrace();
+            }
+        }
+        return ctx;
+    }
+
+}
